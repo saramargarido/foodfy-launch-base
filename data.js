88d7e86e874ffc9d5be50418da92a9cbdc67f084
saramargarido <saramargarido@gmail.com>
2020-07-30 15:42:47 -0300

    taking data from backend to frontend with nunjucks

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "1",
+        img: "/assets/burger.png",
+        title: "Triplo bacon burguer",
+        autor: "Jorge Relato"
+    },
+    {
+        id: "2",
+        img: "/assets/pizza.png",
+        title: "Pizza 4 estações",
+        autor: "Fabiana Melo"
+    },
+    {
+        id: "3",
+        img: "/assets/espaguete.png",
+        title: "Espaguete ao alho",
+        autor: "Júlia Kinoto"
+    },
+    {
+        id: "4",
+        img: "/assets/lasanha.png",
+        title: "Lasanha mac n' cheese",
+        autor: "Juliano Vieira"
+    },
+    {
+        id: "5",
+        img: "/assets/doce.png",
+        title: "Docinhos pão-do-céu",
+        autor: "Ricardo Golveia"
+    },
+    {
+        id: "6",
+        img: "/assets/asinhas.png",
+        title: "Asinhas de frango barbecue",
+        autor: "Vania Steroski"
+    },
+]
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nunjucks = require ('nunjucks')
 
 const server = express()
+const recipes = require("./data")
 
 server.use(express.static('public'))
 
@@ -14,7 +15,7 @@ nunjucks.configure('views', {
 })
 
 server.get("/", function (req, res) {
-    return res.render("home")
+    return res.render("home", { items: recipes })
 })
 
 server.get("/about", function (req, res) {
