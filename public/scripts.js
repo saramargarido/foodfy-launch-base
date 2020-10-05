f8aee5c22d791df19a5d4053f4aa651f1b359daf
saramargarido <saramargarido@gmail.com>
2020-10-04 22:47:16 -0300

    links ativos no menu

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,6 @@
-const modalOverlay = document.querySelector('.modal-overlay')
+// const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
+// const linkRecipes = document.querySelectorAll('.link-recipes')
 
 for (let card of cards) {
     card.addEventListener('click', function(){
@@ -9,24 +10,11 @@ for (let card of cards) {
 }
 
 
-// ==== MODAL ====
 
-// for (let card of cards) {
-//     card.addEventListener('click', function(){
-//         const cardId = card.getAttribute('id')
-//         const cardTitle = card.querySelector('.card h3')
-//         const cardAutor = card.querySelector('.card p')
-//         modalOverlay.classList.add('active')
-//         modalOverlay.querySelector("img").src = `/assets/${cardId}.png`
-//         modalOverlay.querySelector(".modal-content h3").textContent = cardTitle.textContent
-//         modalOverlay.querySelector(".modal-content p").textContent = cardAutor.textContent
-        
-//         })
-// }
 
-// document.querySelector(".close-modal").addEventListener('click', function(){
-//     modalOverlay.classList.remove('active')
-// })
+
+
+
 
 
 
