40fb20422d95aec806ba65ff52ed677e9d93d766
saramargarido <saramargarido@gmail.com>
2021-07-03 19:56:49 -0300

    editando a receita

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,6 +20,10 @@ exports.show = function (req, res) {
     return res.render("admin/recipe", { item: recipe })
 }
 
+exports.create = function (req, res) {
+    return res.render('admin/create')
+}
+
 exports.post = function (req, res) {
     const keys = Object.keys(req.body)
 
@@ -40,6 +44,48 @@ exports.post = function (req, res) {
     })
 }
 
-exports.create = function (req, res) {
-    return res.render('admin/create')
+exports.edit = function (req, res) {
+    // req.params.id = /:id
+    const { id } = req.params
+
+    const foundRecipe = data.recipes.find(function(recipe){
+        return recipe.id == id
+    })
+
+    if (!foundRecipe) return res.send('Recipe not found!')
+    
+    const recipe = {
+        ...foundRecipe,
+    }
+    return res.render('admin/edit', { recipe })
+
 }
+
+exports.put = function (req, res) {
+    const { id } = req.body
+    let index = 0
+
+    const foundRecipe = data.recipes.find(function(recipe, foundIndex){
+        if(id == recipe.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundRecipe) return res.send('Recipe not found!')
+
+    const recipe = {
+        ...foundRecipe,
+        ...req.body,
+    }
+
+    data.recipes[index] = recipe
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send('Write error!')
+
+        return res.redirect(`/admin/recipes/${id}`)
+    })
+}
+
+
--- routes.js
+++ routes.js
@@ -12,10 +12,10 @@ routes.get("/recipes/:id", recipes.show)
 routes.get("/admin/recipes", admin.index); // Mostrar a lista de receitas
 routes.get("/admin/recipes/create", admin.create); // Mostrar formulário de nova receita
 routes.get("/admin/recipes/:id", admin.show); // Exibir detalhes de uma receita
-// routes.get("/admin/recipes/:id/edit", admin.edit); // Mostrar formulário de edição de receita
+routes.get("/admin/recipes/:id/edit", admin.edit); // Mostrar formulário de edição de receita
 
 routes.post("/admin/recipes", admin.post); // Cadastrar nova receita
-// routes.put("/admin/recipes", admin.put); // Editar uma receita
+routes.put("/admin/recipes", admin.put); // Editar uma receita
 // routes.delete("/admin/recipes", admin.delete); // Deletar uma receita
 
 module.exports = routes
