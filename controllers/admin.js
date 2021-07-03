857f51e24f831af57dbe43ec3eeb419e363be7d8
saramargarido <saramargarido@gmail.com>
2021-07-02 22:35:10 -0300

    fields de admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,6 +20,38 @@ exports.show = function (req, res) {
     return res.render("admin/recipe", { item: recipe })
 }
 
+exports.post = function (req, res) {
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        //req.body.key == ""
+        if (req.body[key] == "") {
+            return res.render('Please, fill all fields')
+    }
+}
+
+
+    birth = Date.parse(req.body.birth)
+
+    let id = 1
+    const lastRecipe = data.members[data.members.length - 1]
+
+    if(lastRecipe) {
+        id = lastRecipe.id + 1
+    }
+
+    data.members.push({
+        id,
+        ...req.body,
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Write file error!")
+        
+        return res.redirect("/members")
+    })
+}
+
 exports.create = function (req, res) {
     return res.render('admin/create')
 }
